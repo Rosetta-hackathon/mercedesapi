a48f157c86fca0078a406b52eb3f12d05ecfcd7d
Cag0 <cagatayguzgun@gmail.com>
2018-12-01 21:03:34 +0100

    Valid token

--- main.cpp
+++ main.cpp
@@ -4,16 +4,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-
+#include <iostream>
+#include <jsoncpp/json/json.h>
 #include <curl/curl.h>
 #include <string>
-#define _GLIBCXX_USE_CXX11_ABI 1
+//#define _GLIBCXX_USE_CXX11_ABI 1
 
 
 struct MemoryStruct {
     char *memory;
     size_t size;
 };
+
 
 static size_t
 WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -34,19 +36,24 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
     mem->size += realsize;
     mem->memory[mem->size] = 0;
 
-    return realsize;
+	    return realsize;
 }
-char* client_id = "ODA3ZWI1NDctYjY5My00YjE0LTgwYmItYTU4M2VlN2JiNmZjOjZmYWEzMzkwLTI5ZjgtNGNhYS04OGQzLTkyYjg2YWI4YmEyOQ==";
+char* clientencoded = "ODA3ZWI1NDctYjY5My00YjE0LTgwYmItYTU4M2VlN2JiNmZjOjZmYWEzMzkwLTI5ZjgtNGNhYS04OGQzLTkyYjg2YWI4YmEyOQ==";
 char* Redirect_URLs = "http://localhost";
-	static const char *postthis="grant_type=authorization_code&code=d2dd833b-8c7e-4c01-b867-2080edc253c4&redirect_uri=http://localhost";
+	static const char *postthis="grant_type=authorization_code&code=d17c6cce-fe14-4088-a9af-e247b689ec38&redirect_uri=http://localhost";
 int main(void)//https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/authorize?response_type=code&client_id=807eb547-b693-4b14-80bb-a583ee7bb6fc&redirect_uri=http://localhost&scope=mb:vehicle:status:general mb:user:pool:reader
 
 {
+/*
+curl -X POST "https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/token" \-H "authorization: Basic ODA3ZWI1NDctYjY5My00YjE0LTgwYmItYTU4M2VlN2JiNmZjOjZmYWEzMzkwLTI5ZjgtNGNhYS04OGQzLTkyYjg2YWI4YmEyOQ" \-H "content-type: application/x-www-form-urlencoded" \-d"grant_type=authorization_code&code=bd1ffdc2-95b7-4ab9-9066-addd4eb7eda6&redirect_uri=<insert_redirect_uri_here>"
+*/
+//ACCESS TOKEN 
     CURLcode res;
     CURL *curl_handle;
     struct curl_slist *header= NULL;
     FILE* out =fopen("return.txt","w");
-
+Json::Reader reader;
+    Json::Value obj;
     struct MemoryStruct chunk;
 
     chunk.memory = (char*)malloc(1);  /* will be grown as needed by the realloc above */
@@ -60,12 +67,14 @@ int main(void)//https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/author
     header = curl_slist_append(header,"content-type: application/x-www-form-urlencoded");
 
 
-    	char clid[200];
-	strcat(clid,"authorization: Basic ");
-    	strcat(clid,client_id);
+    	//char clid[200];
+	std::string s;
+s="authorization: Basic ";
+s.append(clientencoded);
+    	
 
 
-    header = curl_slist_append(header,clid);
+    header = curl_slist_append(header,s.c_str());
     res =curl_easy_setopt(curl_handle,CURLOPT_HTTPHEADER, header);
     /* specify URL to get */
     curl_easy_setopt(curl_handle, CURLOPT_URL, "https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/token");
@@ -75,8 +84,7 @@ int main(void)//https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/author
 
     /* we pass our 'chunk' struct to the callback function */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-
-
+//std::cout<<clid;
     curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDS, postthis);//Post field
 
     curl_easy_setopt(curl_handle, CURLOPT_POSTFIELDSIZE, (long)strlen(postthis));//Post size
@@ -99,9 +107,43 @@ int main(void)//https://api.secure.mercedes-benz.com/oidc10/auth/oauth/v2/author
 
         printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
     }
-    printf("%s ", chunk.memory);
+    //printf("%s ", chunk.memory);
+	reader.parse(chunk.memory,obj);
     /* cleanup curl stuff */
     curl_easy_cleanup(curl_handle);
+	std::cout<<chunk.memory<<std::endl;
+	std::cout<<obj["access_token"].asString()<<std::endl;
+
+
+
+//ACCESS TOKEN END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     free(chunk.memory);
     fclose(out);
